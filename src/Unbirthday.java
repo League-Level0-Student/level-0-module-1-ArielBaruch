a8c022108cf9161edba10cd244e710c8205e01c6
League Student <league@iMac-8.attlocal.net>
2019-01-12 17:28:43 -0800

    Module 1

---
+++ src/Unbirthday.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String sentence =	JOptionPane.showInputDialog(null, "When is your birthday");
+		
+	if(sentence.equals("1/12")) {
+		
+		JOptionPane.showMessageDialog(null,  "Happy Birthday");
+		
+		
+	}
+	else {
+		Unhappy Birthday;
+	}
+}
+}
